575e7ed393d9127729a7771ba2d699ceea34699d
Karl Hedin <karl.hedin@jayway.com>
2018-10-11 01:50:13 +0200

    baseline

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import HelloCC from './HelloCC';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+class App extends Component {
+  render() {
+    console.log('hej');
+    return (
+      <Container>
+        <section>
+          Edit <code>src/App.js</code> and save to reload.
+        </section>
+        <section>
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </section>
+        <HelloCC name="Typescript" />
+      </Container>
+    );
+  }
+}
+
+export default App;
